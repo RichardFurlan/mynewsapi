1679c54befb8a8d8732daaf44752f458712271e1
Richard Figueiredo Furlan <emsrichard10@hotmail.com>
2025-08-30 16:46:44 +0200

    chore: remove initial value

--- MyNewsApi/Models/Article.cs
+++ MyNewsApi/Models/Article.cs
@@ -2,7 +2,7 @@
 
 public class Article : BaseEntity
 {
-    public string Title { get; set; } = "";
-    public string Content { get; set; } = "";
+    public string Title { get; set; }
+    public string Content { get; set; }
     public DateTime PublishedAt { get; set; }
 }
